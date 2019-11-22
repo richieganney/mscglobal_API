244f3f389e5b7e7a4f3af60e3df62d91fa8986ce
root <root@ip-10-0-172-16.us-west-2.compute.internal>
2019-11-22 15:57:25 +0000

    adding db model for table

---
+++ api/models/leagueTableModels.js
@@ -0,0 +1,22 @@
+'use strict';
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+'use strict';
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var leagueTable = new Schema({
+    position:  Number,
+    team_name: String,
+    played:   Number,
+    win: Number,
+    draw: Number,
+    loss: Number,
+    points_for: Number,
+    points_against: Number,
+    points_difference: Number,
+    points: Number
+  });
+
+module.exports = mongoose.model('Table', TaskSchema);
