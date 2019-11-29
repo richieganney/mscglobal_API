3d8e5dec4d699107880fea539d7daba412bb1dc1
Richie <richieganney@gmail.com>
2019-11-29 11:44:57 +0000

    intalled body parser and connecting routes and controller in server file

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 var express = require('express'),
   app = express(),
-  port = process.env.PORT || 3000;
+  port = process.env.PORT || 3000,
+  mongoose = require('mongoose'),
+  Table = require('./api/models/leagueTableModel'), //created model loading here
+  bodyParser = require('body-parser');
+  
+// mongoose instance connection url connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/Tabledb'); 
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var routes = require('./api/routes/leagueTableRoutes'); //importing route
+routes(app); //register the route
 
 app.listen(port);
 
